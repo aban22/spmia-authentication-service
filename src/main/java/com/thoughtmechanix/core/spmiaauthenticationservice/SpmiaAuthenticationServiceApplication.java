3056e9366d669279732e6ad18bb511bc15d632d3
Abel Fernandez <abelfergodevelop@gmail.com>
2019-06-21 00:01:06 +0200

    page 221

---
+++ src/main/java/com/thoughtmechanix/core/spmiaauthenticationservice/SpmiaAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.core.spmiaauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmiaAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpmiaAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtmechanix/core/spmiaauthenticationservice/SpmiaAuthenticationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thoughtmechanix.core.spmiaauthenticationservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpmiaAuthenticationServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
